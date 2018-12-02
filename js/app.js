6dc06f8c7e02eac449ce965df39f0f79f38be3dc
Camillo <cangelozzi@gmail.com>
2018-12-02 00:37:58 -0500

    Works section completed

--- js/app.js
+++ js/app.js
@@ -1,69 +1,110 @@
 (() => {
   console.log("Welcome to my Portfolio, let's connect!");
 
+  const flipping = new Flipping();
+
+  const elImages = Array.from(document.querySelectorAll("#myWorks .ui-big-image"));
+  const elThumbnails = Array.from(document.querySelectorAll("#myWorks .ui-thumbnail"));
+
   // VUE instance
   const vm = new Vue({
-
-    el: '#app',
+    el: "#app",
 
     data: {
-      navigationList: ['skills', 'works', 'about', 'contact'],
+      navigationList: ["skills", "works", "about", "contact"],
 
       skills: {
-
         webDevelopment: {
-          title: 'Web Development',
-          desc: 'Experience in Frontend and Backend web development with HTML5 / CSS3, Javascript, NodeJS, VueJS, PHP/MySQL and Laravel.',
-          icon: 'fas fa-terminal'
+          title: "Web Development",
+          desc:
+            "Experience in Frontend and Backend web development with HTML5 / CSS3, Javascript, NodeJS, VueJS, PHP/MySQL and Laravel.",
+          icon: "fas fa-terminal"
         },
 
         webDesign: {
-          title: 'Web Design',
-          desc: 'Approaching digital media design and UX/UI layouts in a clean and dynamic way. Use of Adobe Creative package for the creative mood.',
-          icon: 'fas fa-pencil-alt'
+          title: "Web Design",
+          desc:
+            "Approaching digital media design and UX/UI layouts in a clean and dynamic way. Use of Adobe Creative package for the creative mood.",
+          icon: "fas fa-pencil-alt"
         },
 
         digitalMedia: {
-          title: 'Digital Media',
-          desc: 'Understand that a positive and effective communication involves different types of media including 3D/animations, infographics and video.',
-          icon: 'fas fa-cubes'
+          title: "Digital Media",
+          desc:
+            "Understand that a positive and effective communication involves different types of media including 3D/animations, infographics and video.",
+          icon: "fas fa-cubes"
         },
 
-        futureProof : {
-          title: 'Be Future-Proof',
-          desc: 'Continuous learning is not an option. Try-Learn-Improve-Repeat. Involvement in Blockchain Dev, Web Audio and Generative Art are steps toward the future.',
-          icon: 'far fa-lightbulb'
-        },
-
-        projectsData: []
-
-
-
-      }
+        futureProof: {
+          title: "Be Future-Proof",
+          desc:
+            "Continuous learning is not an option. Try-Learn-Improve-Repeat. Involvement in Blockchain Dev, Web Audio and Generative Art are steps toward the future.",
+          icon: "far fa-lightbulb"
+        }
+      },
+      projectsData: [],
+      state: { photo: 0 },
+      elImages,
+      elThumbnails
     },
 
     created: function() {
+      this.moveImg(0);
       this.fetchProjectsData();
     },
 
     methods: {
-
       fetchProjectsData() {
-        url = './includes/index.php';
+        url = "./includes/index.php";
 
         fetch(url) // pass in the one or many query
           .then(res => res.json())
           .then(data => {
             console.log(data);
-            this.autodata = data;
+            this.projectsData = data;
           })
-          .catch(function (error) {
+          .catch(function(error) {
             console.log(error);
           });
+      },
+
+      moveImg(event) {
+        // read cuticle positions
+        flipping.read();
+
+        const elActives = document.querySelectorAll("[data-active]");
+
+        Array.from(elActives).forEach(el => el.removeAttribute("data-active"));
+
+        switch (event) {
+          case "PREV":
+            this.state.photo--;
+            // Math.max(state.photo - 1, 0);
+            break;
+          case "NEXT":
+            this.state.photo++;
+            // Math.min(state.photo + 1, elImages.length - 1);
+            break;
+          default:
+            this.state.photo = +event;
+            break;
+        }
+
+        var len = this.elImages.length;
+        // Loop Around
+        //state.photo = ( ( state.photo % len) + len ) % len;
+        this.state.photo = Math.max(0, Math.min(this.state.photo, len - 1));
+
+        Array.from(
+          document.querySelectorAll(`[data-key="${this.state.photo}"]`)
+        ).forEach(el => {
+          el.setAttribute("data-active", true);
+        });
+
+        // execute the FLIP animation
+        flipping.flip();
       }
-
     }
-
   });
 
   // LOTTIE bodymovin script
@@ -132,9 +173,6 @@
     }
   }
 
-  // initialize Dom, same as window onload
-  document.addEventListener("DOMContentLoaded", init);
-
   // init app
   function init() {
     const txtElement = document.querySelector(".txt-type");
@@ -144,8 +182,15 @@
     // initialize type writer
     setTimeout(() => {
       new TypeWriter(txtElement, words, wait);
-    }, 4800);
+    }, 3600);
   }
-    // -------- end PRESENTATION TYPEWRITER EFFECT ----- //
+  // -------- end PRESENTATION TYPEWRITER EFFECT ----- //
 
+  // initialize Dom, same as window onload
+  document.addEventListener("DOMContentLoaded", init);
+
+  // reload page always on top
+  window.onbeforeunload = function() {
+    window.scrollTo(0, 0);
+  };
 })();
