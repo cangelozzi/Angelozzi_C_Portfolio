3332938cd2ed7cfb271db31c449fc1119dcf85ed
Camillo <cangelozzi@gmail.com>
2019-03-23 14:11:37 -0400

    query update

--- includes/functions.php
+++ includes/functions.php
@@ -4,18 +4,17 @@ require 'connect.php';
 // get all projects and category with INNER JOIN via the linking table
 function get_projects($pdo)
 {
-  // create a query
-  $query = "SELECT p.*, c.category_name FROM tbl_projects p INNER JOIN tbl_proj_cat l INNER JOIN tbl_categories c ON p.project_id = l.project_id AND l.category_id = c.category_id;";
-  
-  // pass query to connection
-  $get_projects = $pdo->query($query);
+    // create a query
+    $query = "SELECT p.*, c.category_name FROM tbl_projects p INNER JOIN tbl_proj_cat l INNER JOIN tbl_categories c ON p.project_id = l.project_id AND l.category_id = c.category_id;";
 
-  // add fetched result to array
-  $results = array();
-  while ($row = $get_projects->fetch(PDO::FETCH_ASSOC)) {
-    $results[] = $row;
-  }
-  return $results;
+    // pass query to connection
+    $get_projects = $pdo->prepare($query);
+    $get_projects->execute();
+
+    // add fetched result to array
+    $results = array();
+    while ($row = $get_projects->fetch(PDO::FETCH_ASSOC)) {
+        $results[] = $row;
+    }
+    return $results;
 }
-
-?>
